f79e387df89842fc05b8d6b790204a411285601a
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-14 18:32:44 -0300

    explicando arrow functions

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-function App() {
-  return <Text>Primeiro Component</Text>;
-}
-export default App;
+export default () => <Text>Primeiro Component</Text>;
