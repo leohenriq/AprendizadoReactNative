7c47a07199051dd88094eb86482d4d83d975dfe2
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-14 18:46:37 -0300

    multiplos exports

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 
 import Primeiro from './components/Primeiro';
+import CompPadrao, {Comp1, Comp2} from './components/Multi';
 
 export default () => (
   <View>
+    <CompPadrao />
+    <Comp1 />
+    <Comp2 />
     <Primeiro />
   </View>
 );
