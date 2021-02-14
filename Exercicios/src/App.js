3d032f62424689755fc801272e22962c2ec34537
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-14 20:01:12 -0300

    desafio numeros aleatorios

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-
+import Aleatorio from './components/Aleatorio';
 // import Primeiro from './components/Primeiro';
 // import CompPadrao, {Comp1, Comp2} from './components/Multi';
 import MinMax from './components/MinMax';
 export default () => (
   <View style={style.App}>
-    {/* passando entre {} é entendido como JS
+    <Aleatorio min={5} max={10} />
+    {/* EXPLICAÇÃO:
+    passando entre {} é entendido como JS
     portanto, o texto 3 dentro das chaves se torna
     numero e com aspas se torna texto */}
-    <MinMax min={3} max={20} />
-    <MinMax min="5" max="40" />
+    {/* <MinMax min={3} max={20} />
+    <MinMax min="5" max="40" /> */}
     {/* <CompPadrao />
     <Comp1 />
     <Comp2 />
