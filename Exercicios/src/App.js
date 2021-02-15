da1e47a8854687bb5d7ecd78b7871f170cb8d2ad
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-15 20:36:47 -0300

    button

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import Titulo from './components/Titulo';
+import Button from './components/Button';
+// import Titulo from './components/Titulo';
 // import Aleatorio from './components/Aleatorio';
 // import Primeiro from './components/Primeiro';
 // import CompPadrao, {Comp1, Comp2} from './components/Multi';
 // import MinMax from './components/MinMax';
 export default () => (
   <View style={style.App}>
-    <Titulo principal="1234" secundario="4321" />
+    <Button />
+    {/* <Titulo principal="1234" secundario="4321" /> */}
     {/* <Aleatorio min={5} max={10} /> */}
     {/* EXPLICAÇÃO:
     passando entre {} é entendido como JS
