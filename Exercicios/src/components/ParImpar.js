fa9c4548cd88d77368edf1095ae25627ce66f4e1
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-16 16:46:45 -0300

    parimpar

--- Exercicios/src/components/ParImpar.js
+++ Exercicios/src/components/ParImpar.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import Estilo from './estilo';
-export default ({num = 0}) => {
+export default ({ num = 0 }) => {
   return (
     <View>
       <Text style={Estilo.txtG}>O resultado é</Text>
-      {num % 2 === 0 ? (
-        <Text style={Estilo.txtG}>Par</Text>
-      ) : (
-        <Text style={Estilo.txtG}>Ímpar</Text>
-      )}
+      {num % 2 === 0
+        ? <Text style={Estilo.txtG}>Par</Text>
+        : <Text style={Estilo.txtG}>Ímpar</Text>
+      }
       <Text style={Estilo.txtG}>{num % 2 === 0 ? 'Par' : 'Ímpar'}</Text>
     </View>
   );
