73e1b086bc70b9fad4f11619b3ce27334a815af8
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-20 20:54:02 -0300

    class state

--- Exercicios/src/components/mega/Mega.js
+++ Exercicios/src/components/mega/Mega.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Fragment } from 'react';
+import { Text, TextInput } from 'react-native';
 import Estilo from '../estilo'
 export default class Mega extends React.Component {
+    state = {
+        qtdeNumeros: this.props.qtdeNumeros
+    }
 
+    alterarQtdeNumero(qtde) {
+        this.setstate({ qtdeNumeros: qtde })
+    }
 
     // funcao q vai renderizar os itens na tela
     render() {
         return (
-            <Text style={Estilo.txtG}>
-                Gerador de Mega-Sena
-                {this.props.qtdeNumeros}
-            </Text>
+            <Fragment>
+                <Text style={Estilo.txtG}>
+                    Gerador de Mega-Sena
+                {this.state.qtdeNumeros}
+                </Text>
+                <TextInput
+                    placeholder="Qtde de Números"
+                    value={this.state.qtdeNumeros}
+                    onChangeText={this.alterarQtdeNumero}
+                />
+            </Fragment>
         )
     }
 }
