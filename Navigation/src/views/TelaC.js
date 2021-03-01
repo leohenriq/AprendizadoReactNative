dc2fb0d71eab06c71facc08a45cdc160e8397bae
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-03-01 11:12:51 -0300

    telas

--- Navigation/src/views/TelaC.js
+++ Navigation/src/views/TelaC.js
@@ -1,1 +1,9 @@
 
+import React from 'react';
+import TextoCentral from '../components/TextoCentral'
+
+export default props => {
+    <TextoCentral corFundo="#9932cd">
+        TelaC
+    </TextoCentral>
+}
