0927be3844a361a9543817c5418b401dbd7e08ee
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2021-02-28 21:44:55 -0300

    campo minado

---
+++ mines/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+import params from './src/params'
+import Field from './src/components/Field'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da grade:
+        {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+        <Field />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
